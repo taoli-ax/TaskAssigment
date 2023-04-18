da3b430992cc09aef5fc250477636b149bf6bfcd
taoli-ax <deuta-tao@outlook.com>
2023-04-18 08:57:17 +0800

    add hibernateValidation

--- src/main/java/com/coh/controller/OrderController.java
+++ src/main/java/com/coh/controller/OrderController.java
@@ -4,6 +4,9 @@ import com.coh.service.OrderService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,9 +20,17 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
     @RequestMapping("new")
-    public @ResponseBody Map<String,Object> newOrder(Orders order){
+    @ResponseBody
+    public Map<String,Object> newOrder(@Validated Orders order, Errors errors){
         System.out.println(order);
         Map<String,Object>map=new HashMap<>();
+        if(errors!=null&&errors.getFieldErrors().size()>0){
+            for(FieldError fieldError:errors.getFieldErrors()){
+                map.put(fieldError.getField()+"Message",fieldError.getDefaultMessage());
+            }
+            map.put("result",false);
+            return map;
+        }
         order.setCreate_at(new Date());
         try {
             orderService.create(order);
--- src/main/java/com/coh/pojo/Orders.java
+++ src/main/java/com/coh/pojo/Orders.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 @Data
 @AllArgsConstructor
@@ -11,7 +12,9 @@ import java.util.Date;
 public class Orders {
     private int id;
     private int project_id;
+    @NotBlank(message = "创建人不能为空")
     private String creator;
+    @NotBlank(message = "描述信息不能为空")
     private String description;
     private int priority;
     private Date create_at;
